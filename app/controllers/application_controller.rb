c582de35732fb50c29c7f069692db4c52e3bbb9f
tcharfi66 <charfitej6@gmail.com>
2020-09-30 12:56:15 -0400

    ...

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,25 @@
 class ApplicationController < ActionController::Base
+   
+    helper_method :logged_in?, :current_user
     def about
         @num_clients = Client.count 
         @num_admins= Admin.count
         render :about
     end 
+
+    def logged_in?
+        !!session[:user_id]
+    end
+
+    def current_user
+        if logged_in?
+            return User.find(session[:user_id])
+        else
+            return nil
+        end
+    end
+
+    def authorized
+        redirect_to login_path unless logged_in?
+    end
 end

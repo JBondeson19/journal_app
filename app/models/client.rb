edfdd2241d0ccc280b40e723066779a89fe0573f
tcharfi66 <charfitej6@gmail.com>
2020-10-02 10:17:56 -0400

    delete fixed

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,6 +1,6 @@
 class Client < ApplicationRecord
-    has_many :client_admins
-    has_many :admins, through: :client_admins 
+    has_many :client_admins, dependent: :destroy
+    has_many :admins, through: :client_admins
     
 
     validates :age, numericality: {greater_than: 18, less_than_or_equal_to: 100  }
